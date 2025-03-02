c1ff5f0002d32cf149cf9c15c3f2e05153496a69
Feelingsheepy <dylanhusted@hotmail.com>
2025-03-02 21:10:00 +0200

    Fixed some mistakes in the experience section

--- app/components/Resume/Experience.vue
+++ app/components/Resume/Experience.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 const experienceItems = ref([
   {
-    label: 'Unemployed',
+    label: 'Career Break',
     date: '2023 Oct',
     dateEnd: 'present',
     descriptions: [
       `We were hosting family members from overseas for a few months after I finished my previous job. 
         Then I had booked a vacation to go visit family in Australia for a couple of months. I did not 
         want to try look for a job before the visit to Australia because then I would have limited my 
-        options to companies that would allow me to take such a long break after having just started working.`
+        options to companies that would allow me to take such a long break after having just started working.`,
+      `I have been actively searching since January 2025. I would like to get hired by a company that I can
+        work at for a longer period of time and grow there. Therefore, I would rather take my time and find
+        a job that suits me well.`,
     ],
   },
   {
@@ -16,9 +19,12 @@ const experienceItems = ref([
     date: '2021 Mar',
     dateEnd: '2023 Oct',
     position: 'Tech Lead',
-    technology: 'Python, JavaScript, SQL, Azure Cloud Stack, SendGrid, NodeJS, Vue, Docker, Git',
+    technology: 'Python, JavaScript, SQL, Azure Cloud Stack, SendGrid, NodeJS, Vue, Docker, Git, CSS, HTML',
     descriptions: [
       `This was a small start-up located in Auckland, New Zealand. I worked remotely.`,
+      `UserOrg was an operations insight platform that allowed employees to review systems, policies, processes and other employees
+        within the company. This information was used to provide feedback to the employees themselves on how to improve and where
+        they were lacking. As well as providing feedback to the managers to see where the problems and bottlenecks are.`,
       `I was employed at this start-up to help convert a skeleton web application into a fully functioning, cloud-hosted web application.`,
       `This required re-factoring the code, setting up the git repository, deploying the code via docker and then using the docker to
         deploy the code on Azure App Services, integrating email capabilities using SendGrid, migrating the local SQL database to Azure
@@ -26,11 +32,12 @@ const experienceItems = ref([
       `After this was in place, I saw to improving the efficiency of the SQL operations, setting up database/cloud alarms, improving
         the work-flow between developer and stakeholder with an issue tracking platform, automatic builds and deployment with a dev-ops
         platform. Then after that I worked on maintaining the application, fixing bugs, improving the UX aspects of the web app, adding
-        features requested by the stakeholders, giving advice on possible solutions, providing estimates and forecasts for future development work.`
+        features requested by the stakeholders, giving advice on possible solutions, providing estimates and forecasts for future
+        development work.`
     ],
   },
   {
-    label: 'Unemployed',
+    label: 'Personal Project',
     date: '2018 Sep',
     dateEnd: '2021 Mar',
     technology: 'Vue, CosmosDB, MariaDB, GraphQL/GremlinDB, NodeJS, TensorFlow, Docker, Kubernetes, Function Apps, Event-based architecture, Message Queues and Publisher/Subscriber flows, WebSockets',
@@ -45,20 +52,22 @@ const experienceItems = ref([
     label: 'Skynamo',
     date: '2015 Nov',
     dateEnd: '2018 Sep',
-    position: 'Junior Developer to Intermediate Developer to Lead on a small team',
-    technology: 'C#, Javascript, AWS CloudStack, Git, Java, Android/iOS',
+    position: 'Junior Developer to Lead on a small team',
+    technology: 'C#, Javascript, AWS CloudStack, Git, Java, Android/iOS, CSS, HTML',
     descriptions: [
+      `Skynamo is a CRM which helps companies oversee their sales reps in the field. Giving the managers an overview into
+        what is happening in the field and the reps the ability to submit important business information while on the go.`,
       `I started at Skynamo as a Junior Developer. I initially worked mostly on fixing bugs and implementing small features
         as well as learning under the Lead Developer.`,
       `Then, because of my success, I was given a project to work on by myself as well as the responsibility to correspond
         with the client on that project. That project involved getting our web app to function entirely on the client's on-site infrastructure.`,
-      `Afterwards I continued to help with features and bug fixing on the main project, mostly back-end and front-end of the web development, 
+      `After 12 months I was promoted and I continued to help with features and bug fixing on the main project, mostly back-end and front-end of the web development,
         but sometimes I would help with mobile features/bugs on Android and iOS.`,
       `I was also tasked with converting the version control and DevOps to Git to help with work-flow and automatic deployment/testing(CI).
         As well as being the admin and maintainer of the version control and DevOps, with input from the Lead Developer and other developers
         to ensure we were all on the same page. I was also tasked with refining our development processes by speaking with the other developers
-        and figuring out what systems and processes would be suit our changing team and helping the project manager to implement these new processes. `,
-      `Eventually, I was made the lead developer of a small team to pursue features adjacent to the main project of the company. This included helping a
+        and figuring out what systems and processes would suit our changing team and helping the project manager to implement these new processes.`,
+      `After 2 years, I was promoted to the lead developer of a small team to pursue features adjacent to the main project of the company. This included helping a
         junior developer to adjust to working in our company and corresponding with our project manager to fulfil the needs of our team.`,
     ],
   },
@@ -75,7 +84,7 @@ const experienceItems = ref([
     date: '2015 Jan',
     dateEnd: '2015 July',
     position: 'Contracted Intern (6 months)',
-    technology: 'Delphi, C++, C# and Python, Git',
+    technology: 'Delphi, C++, C#, Python, Git',
     descriptions: [
       `I worked under a senior developer to help create the new desktop application that was to be used by clients of the Mining
         Department. It was basically an application that took the raw input from the radar and converted it to human understandable
@@ -91,7 +100,7 @@ const experienceItems = ref([
     date: '2012 Jun',
     dateEnd: '2014 Dec',
     position: 'Intern (23 weeks in period)',
-    technology: 'Delphi, C++, C# and Python, Git, openGL',
+    technology: 'Delphi, C++, C#, Python, Git, openGL',
     descriptions: [
       `I interned during my university holidays in order to get some experience before I finished my degree. I worked in openGL
         and Delphi to implement small features for the 3d rendering application used to display open pit mines to clients. I modified and fixed
